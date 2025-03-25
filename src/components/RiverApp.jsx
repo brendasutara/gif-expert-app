0bf3aaf3ff3bb4d68834546b10d2eb090f36dd13
Brenda Sutara <sutarabrendagmail.com>
2025-03-24 22:31:57 -0300

    Acercate

--- src/components/RiverApp.jsx
+++ src/components/RiverApp.jsx
@@ -1,11 +1,21 @@
 import Rive from "@rive-app/react-canvas";
 
 export const RiveDemo = () => (
-  <Rive
-    src="/caminando-t.riv"
-    stateMachines="State Machine 1"
+  <div
     style={{
-      height: "500px",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
     }}
-  />
+  >
+    <p>Acercate, mira como camino 👇</p>
+    <Rive
+      src="/caminando-t.riv"
+      stateMachines="State Machine 1"
+      style={{
+        height: "500px",
+      }}
+    />
+  </div>
 );
